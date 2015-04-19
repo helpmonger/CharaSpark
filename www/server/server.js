97ed1d9692be64bc9396bedba30904315c935340
david <monger@davids-MacBook-Pro.local>
2015-04-19 02:51:11 -0400

    added additional donation functions

--- www/server/server.js
+++ www/server/server.js
@@ -30,6 +30,12 @@ server.get({path : PATH + 'token' , version : '0.0.1'} , getToken);
 server.post({path : PATH +'processPayment' , version : '0.0.1'} , processPayment);
 server.post({path : PATH +'SignUp', version: '0.0.1'} , SignUp);
 server.post({path : PATH +'LogIn', version: '0.0.1'} , LogIn);
+server.post({path : PATH +'AddWish', version: '0.0.1'} , AddWish);
+server.post({path : PATH +'GetUserWishes', version: '0.0.1'} , GetUserWishes);
+server.post({path : PATH +'GetUserDonations', version: '0.0.1'} , GetUserDonations);
+server.post({path : PATH +'GetUserFulfillments', version: '0.0.1'} , GetUserFulfillments);
+server.post({path : PATH +'GetPaidWishes', version: '0.0.1'} , GetPaidWishes);
+
 // server.post({path : PATH +'Jobs', version: '0.0.1'} ,postNewJob);
 // server.del({path : PATH +'/:jobId' , version: '0.0.1'} ,deleteJob);
 
@@ -108,12 +114,9 @@ function SignUp(req , res , next){
 
 function LogIn(req , res , next){
     var user = {};
-    //user.password = req.params.password;
-    //user.postedOn = new Date();
  
     res.setHeader('Access-Control-Allow-Origin','*');
  
-    //user.user_name = req.params.user_name;
     console.log('User name is: ', req.params.user_name);
     var userfromdb;
     users.findOne({user_name:req.params.user_name}, function(err, data){
@@ -147,7 +150,130 @@ function LogIn(req , res , next){
 
 
 }
+
+
     
+function AddWish(req , res , next){
+    req.params.postedOn = new Date();
+    res.setHeader('Access-Control-Allow-Origin','*');
+    wishes.save(req.params , function(err , success){
+        console.log('Response success ', success);
+        console.log('Response error ', err);
+        if(success){
+            res.send(201 , wishes);
+            return next();
+        }else{
+            return next(err);
+        }
+    });
+}
+
+
+function GetUserWishes(req , res , next){
+    var user = {};
+ 
+    res.setHeader('Access-Control-Allow-Origin','*');
+ 
+    console.log('User id is: ', req.params._id);
+
+    //1. get id from request parameter
+    //2. find wishes by passing id of request parameter to the user collection 
+    //3. handle the success and failure from the find method
+    //4. call res.send to return the json data of the wishes for that user id
+
+    //var userwishesfromdb;
+
+        wishes.find({userid:req.params._id}, function(err, data){
+    	if(err){
+    		console.log('err is: ', err);
+    		return next(err);
+    	}
+    	else {
+    		//userwishesfromdb = data;
+    		console.log('data is ', data);
+    		res.send(201 , data);
+
+    	}
+    });
+}
+
+function GetUserDonations(req , res , next){
+    var user = {};
+ 
+    res.setHeader('Access-Control-Allow-Origin','*');
+ 
+    console.log('User id is: ', req.params._id);
+
+
+        wishes.find({userid:req.params._id}, function(err, data){
+    	if(err){
+    		console.log('err is: ', err);
+    		return next(err);
+    	}
+    	else {
+    		console.log('data is ', data);
+    		if(data.amount > 0)
+    		{
+    		res.send(201 , data);
+    	}
+
+    	}
+    });
+} 
+
+
+function GetUserFulfillments(req , res , next){
+    var user = {};
+ 
+    res.setHeader('Access-Control-Allow-Origin','*');
+ 
+    console.log('User id is: ', req.params._id);
+
+
+        wishes.find({userid:req.params._id}, function(err, data){
+    	if(err){
+    		console.log('err is: ', err);
+    		return next(err);
+    	}
+    	else { 
+    		console.log('data is ', data);
+    		if(data.wishstatus == "Fulfilled")
+    		{
+    		res.send(201 , data);
+    	}
+
+    	}
+    });
+} 
+
+
+function GetPaidWishes(req , res , next){
+    var user = {};
+ 
+    res.setHeader('Access-Control-Allow-Origin','*');
+ 
+    console.log('User id is: ', req.params._id);
+
+
+        wishes.find({userid:req.params._id}, function(err, data){
+    	if(err){
+    		console.log('err is: ', err);
+    		return next(err);
+    	}
+    	else { 
+    		console.log('data is ', data);
+    		if(data.donationstatus == "Completed")
+    		{
+    		res.send(201 , data);
+    	}
+
+    	}
+    });
+} 
+
+
+
+
 
 
 
