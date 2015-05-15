4df50f9cd1f33feaf09718c881125b506314cb8e
davidhe <beetz12@gmail.com>
2015-05-14 23:44:26 -0400

    removed ds_store

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -42,12 +42,7 @@ myApp.controller('MyWishesCtrl', function($scope, $state, WishService, $localSto
 
 		
 		
-	$scope.goToDetails = function(wish){
-		//alert('in details');
-		$localStorage.wish = wish;
-		$state.go('tab.mywishdescription');
-		//  {'id': '101'}
-	}
+	
 	
 })
 
@@ -247,57 +242,7 @@ var geoLoc = {
 
 
 //tab-fullfillawish
-.controller('FullfillaWishCtrl', function($scope,$state, WishService, $localStorage) {
-	
-	// var promise = WishService.getWishesToFulfill({_id: 'jsfd'});
- //  	promise.then(function(wishes, err) {
- //    // returns a list of users
- //    if(!err){
- //      // console.log('list is: ', wishes);
- //      $scope.wishes = wishes;
- //      console.log('wishes ', $scope.wishes);
- //    }
- //    else {
- //      console.log('error is: ', err);
- //    }
 
- //  }); // end of promise 
-
-		 $scope.wishes=[
-		
-			{	'id': '103',
-				'title':'Wedding Accompany',	
-				'charity':'Salvation Army',
-				'amount':5,
-				'date':'4/10/15',
-				'description':'I\'m going to a friend\'s wedding next Saturday, but I hate to be there alone. Looking for a female to come with me',
-				'status':'new',
-				'donor':'David',
-				'fulfiller':'',
-				'distance':1.5
-			},
-			{	'id': '104',
-				'title':'Resume Help',	
-				'charity':'Learning for Life',
-				'amount':5,
-				'date':'4/10/15',
-				'description':'Can somebody help with reviewing my resume? Any suggestions welcome!',
-				'status':'new',
-				'donor':'David',
-				'fulfiller':'',
-				'distance':2.2
-			},
-
-		];
-
-	$scope.goToDetails = function(wish){
-		//alert('in details');
-		$localStorage.wish = wish;
-		$state.go('tab.wishdescription');
-		//  {'id': '101'}
-	}
-	
-})	
 
 .controller('WishDescriptionCtrl', function($scope,$state, $localStorage) {
 	$scope.aWish= $localStorage.wish;
