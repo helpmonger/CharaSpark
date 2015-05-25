7e54c06de48b175943d5012895e6fcd6e7be38cd
david <beetz12@gmail.com>
2015-05-25 17:08:51 -0400

    updated controllers

--- www/controllers/fulfillwish/FulfillWishCtrl.js
+++ www/controllers/fulfillwish/FulfillWishCtrl.js
@@ -1,33 +1,5 @@
 myApp.controller('FulfillWishCtrl', function($scope,$state, WishService, $localStorage) {
 	
-	
-		//  $scope.wishes=[
-		
-		// 	{	'id': '103',
-		// 		'title':'Wedding Accompany',	
-		// 		'charity':'Salvation Army',
-		// 		'amount':5,
-		// 		'date':'4/10/15',
-		// 		'description':'I\'m going to a friend\'s wedding next Saturday, but I hate to be there alone. Looking for a female to come with me',
-		// 		'status':'new',
-		// 		'donor':'David',
-		// 		'fulfiller':'',
-		// 		'distance':1.5
-		// 	},
-		// 	{	'id': '104',
-		// 		'title':'Resume Help',	
-		// 		'charity':'Learning for Life',
-		// 		'amount':5,
-		// 		'date':'4/10/15',
-		// 		'description':'Can somebody help with reviewing my resume? Any suggestions welcome!',
-		// 		'status':'new',
-		// 		'donor':'David',
-		// 		'fulfiller':'',
-		// 		'distance':2.2
-		// 	},
-
-		// ];
-
 	$scope.goToDetails = function(wish){
 		alert('in details');
 		$localStorage.wish = wish;
--- www/controllers/myfulfillments/MyFulfillmentsCtrl.js
+++ www/controllers/myfulfillments/MyFulfillmentsCtrl.js
@@ -1,35 +1,46 @@
 //Fullfillments data needs to be updated to Fullfillments.
-myApp.controller('MyFullfillmentsCtrl', function($scope, $state, $localStorage) {
+myApp.controller('MyFullfillmentsCtrl', function($scope, $state, WishService, $localStorage) {
 
-	$scope.wishes = //$localStorage.wishes;
-		{
+	var promise = WishService.findWishesFromFulfiller();
+	promise.then(function(wishes,err){
+		if(!err){
+	      $scope.wishes = wishes;
+	      console.log('wishes ', $scope.wishes);
+	    }
+	    else {
+	      console.log('error is: ', err);
+	    }
+	});
+
+	// $scope.wishes = //$localStorage.wishes;
+	// 	{
 			
-		       "101":{				
-				'_wishMaker':'Daniel',
-				'_fulfiller':'Lydia',
-				'_charity':'Salvation Army',
-				'title':'Meal Partner',	
-				'description':'I\'m new to the city. Is there anyone who want to come and hangout with me? I\'ll pay.',
-				'wishStatus':'Cancelled',
-				'createdDate':'4/10/15',
-				'location':[28.222222,39.999999],
-				'startDate':'4/10/15',
-				'expireDate':'5/10/15'
-			},
-	        	"102":{
-				'_wishMaker':'Daniel',
-				'_fulfiller':'Lydia',
-				'_charity':'Salvation Army',
-				'title':'Laptop Needs To Be Fixed',	
-				'description':'My laptop is broken. I feel bad. Can somebody help?',
-				'wishStatus':'Cancelled',
-				'createdDate':'4/11/15',
-				'location':[28.222222,39.999999],
-				'startDate':'4/11/15',
-				'expireDate':'5/11/15'
-			}
+	// 	       "101":{				
+	// 			'_wishMaker':'Daniel',
+	// 			'_fulfiller':'Lydia',
+	// 			'_charity':'Salvation Army',
+	// 			'title':'Meal Partner',	
+	// 			'description':'I\'m new to the city. Is there anyone who want to come and hangout with me? I\'ll pay.',
+	// 			'wishStatus':'Cancelled',
+	// 			'createdDate':'4/10/15',
+	// 			'location':[28.222222,39.999999],
+	// 			'startDate':'4/10/15',
+	// 			'expireDate':'5/10/15'
+	// 		},
+	//         	"102":{
+	// 			'_wishMaker':'Daniel',
+	// 			'_fulfiller':'Lydia',
+	// 			'_charity':'Salvation Army',
+	// 			'title':'Laptop Needs To Be Fixed',	
+	// 			'description':'My laptop is broken. I feel bad. Can somebody help?',
+	// 			'wishStatus':'Cancelled',
+	// 			'createdDate':'4/11/15',
+	// 			'location':[28.222222,39.999999],
+	// 			'startDate':'4/11/15',
+	// 			'expireDate':'5/11/15'
+	// 		}
 
-		}
+	// 	}
 	
 	$scope.goToDetails = function(wish){
 		//alert('in details');
