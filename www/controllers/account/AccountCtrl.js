c51c6226c44909f8afc04da1da5b6f35667eec23
mzdu <daniel_pub@hotmail.com>
2015-05-31 15:12:16 -0400

    populated user information to account setting page.

--- www/controllers/account/AccountCtrl.js
+++ www/controllers/account/AccountCtrl.js
@@ -1,30 +1,30 @@
-myApp.controller('AccountCtrl', function($scope,$state, StorageService, DonationService) {
+myApp.controller('AccountCtrl', function($scope,$state, $localStorage, StorageService, DonationService) {
 
-	$scope.settings = {
-			enableFriends: true
-    }
-
-    console.log("account controller here");
+	// assign the user value from localStorage to local scope
+	var user = $localStorage.user.user;
+	$scope.user = user;
+	
 	$scope.changePassword = function(){
 		//alert('in details');
 		$state.go('tab.changepassword');
 		//  {'id': '101'}
-	}
+	};
 
 	$scope.logOff = function(){
 		StorageService.resetCurrentUser();
 		$state.go('login', {}, { reload: true });
-	}
+	};
 
-	var promise = DonationService.findDonationsFromUser();
-	promise.then(function(results,err){
-		if(!err){
-	      $scope.donations = results;
-	      $scope.totalDonation = results.totalDonation;
-	      console.log('donations ', $scope.donations);
-	    }
-	    else {
-	      console.log('error is: ', err);
-	    }
-	});
-})
+//	var promise = DonationService.findDonationsFromUser();
+//	promise.then(function(results,err){
+//		if(!err){
+//	      $scope.donations = results;
+//	      $scope.totalDonation = results.totalDonation;
+//	      console.log('donations ', $scope.donations);
+//	    }
+//	    else {
+//	      console.log('error is: ', err);
+//	    }
+//	});
+	
+}) // end of AccountCtrl
