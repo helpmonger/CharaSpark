e0e216757874c215d7a9e634b4a20ae33fee64d9
david <beetz12@gmail.com>
2015-05-31 20:26:26 -0400

    fixed fulfill state in tabs.html

--- www/controllers/myfulfillments/MyFulfillmentsCtrl.js
+++ www/controllers/myfulfillments/MyFulfillmentsCtrl.js
@@ -1,6 +1,6 @@
 //Fullfillments data needs to be updated to Fullfillments.
 
-myApp.controller('MyFullfillmentsCtrl', function($scope, $state, WishService, StorageService) {
+myApp.controller('MyFulfillmentsCtrl', function($scope, $state, WishService, StorageService) {
 console.log('in MyFullfillmentsCtrl');
 	var user = StorageService.getCurrentUser();
 		console.log('the user id is: ', user.user._id);
@@ -18,35 +18,6 @@ console.log('in MyFullfillmentsCtrl');
 		});
 	}
 
-	// $scope.wishes = //$localStorage.wishes;
-	// 	{
-			
-	// 	       "101":{				
-	// 			'_wishMaker':'Daniel',
-	// 			'_fulfiller':'Lydia',
-	// 			'_charity':'Salvation Army',
-	// 			'title':'Meal Partner',	
-	// 			'description':'I\'m new to the city. Is there anyone who want to come and hangout with me? I\'ll pay.',
-	// 			'wishStatus':'Cancelled',
-	// 			'createdDate':'4/10/15',
-	// 			'location':[28.222222,39.999999],
-	// 			'startDate':'4/10/15',
-	// 			'expireDate':'5/10/15'
-	// 		},
-	//         	"102":{
-	// 			'_wishMaker':'Daniel',
-	// 			'_fulfiller':'Lydia',
-	// 			'_charity':'Salvation Army',
-	// 			'title':'Laptop Needs To Be Fixed',	
-	// 			'description':'My laptop is broken. I feel bad. Can somebody help?',
-	// 			'wishStatus':'Cancelled',
-	// 			'createdDate':'4/11/15',
-	// 			'location':[28.222222,39.999999],
-	// 			'startDate':'4/11/15',
-	// 			'expireDate':'5/11/15'
-	// 		}
-
-	// 	}
 	
 	$scope.goToDetails = function(wish){
 		$state.go('tab.myfulfillmentdescription',{'wishID': wish._id});
